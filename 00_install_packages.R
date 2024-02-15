b971210485eb4606b28e606a89f62445a3b19c73
Victor Leandro-Silva <31246432+VictorLeandroSilva@users.noreply.github.com>
2024-02-15 15:28:09 -0300

    Add files via upload

---
+++ 00_install_packages.R
@@ -0,0 +1,35 @@
+### install packages enm ###
+
+# occurrences
+if(!require(mapr)) install.packages("mapr")
+if(!require(spocc)) install.packages("spocc")
+if(!require(sf)) install.packages("sf")
+if(!require(tidyverse)) install.packages("tidyverse")
+if(!require(ggmap)) install.packages("ggmap")
+if(!require(rgbif)) install.packages("rgbif")
+
+# variables
+if(!require(caret)) install.packages("caret")
+if(!require(colorRamps)) install.packages("colorRamps")
+if(!require(corrplot)) install.packages("corrplot")
+if(!require(factoextra)) install.packages("factoextra")
+if(!require(FactoMineR)) install.packages("FactoMineR")
+if(!require(maps)) install.packages("maps")
+if(!require(maptools)) install.packages("maptools")
+if(!require(psych)) install.packages("psych")
+if(!require(raster)) install.packages("raster")
+if(!require(rgdal)) install.packages("rgdal")
+if(!require(RStoolbox)) install.packages("RStoolbox")
+if(!require(spatstat)) install.packages("spatstat")
+if(!require(usdm)) install.packages("usdm")
+
+# algorithms
+if(!require(dismo)) install.packages("dismo")
+if(!require(kernlab)) install.packages("kernlab")
+if(!require(randomForest)) install.packages("randomForest")
+
+# ensembles
+if(!require(data.table)) install.packages("data.table")
+
+###---------------------------------------------------------------------------###
+
